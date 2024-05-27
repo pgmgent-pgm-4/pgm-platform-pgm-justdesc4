3d16f508a1e0ef29d19214e4a1c9dda1573a4de2
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2024-05-27 19:31:31 +0200

    WIP portfolio page - added reset.css for stylings

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <div>
+      <h1>Ontdek ons werk.</h1>
+      <figure>
+        <img src="/assets/images/projects-banner.png" alt="Projects banner" />
+      </figure>
+    </div>
+  );
 }
